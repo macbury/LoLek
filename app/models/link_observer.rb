18ae9874aa3fd8f469a0446444fd4767305ca3cd
macbury <macbury@gmail.com>
2012-01-09 13:59:15 +0100

    fix

--- app/models/link_observer.rb
+++ app/models/link_observer.rb
@@ -2,6 +2,7 @@ class LinkObserver < Mongoid::Observer
   observe :link
 
   def after_create(link)
+    return if link.user.nil?
     Delayed::Job.enqueue LinkObserverWorker.new(link), priority: Delay::Observer
   end
 end
